fae30c4d9b7307a8ee9f9cfa02c34b0737d6748d
Sahil-pillania <sahilpillania88@gmail.com>
2022-01-18 19:34:54 +0530

    Operator Overloading

---
+++ operatorOverloadingBinary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Complex
+{
+    double x;
+    double y;
+    public Complex()
+    {
+
+    }
+    public Complex(double real, double imag)
+    {
+        x = real;
+        y = imag;
+    }
+    public static Complex operator +(Complex c1, Complex c2)
+    {
+        Complex c3 = new Complex();
+        c3.x = c1.x + c2.x;
+        c3.y = c1.y + c2.y;
+        return (c3);
+    }
+    public void Display()
+    {
+        Console.Write(x);
+        Console.Write(" +j" + y);
+        Console.WriteLine();
+
+
+    }
+}
+namespace advanced
+{
+    class operatorOverloadingBinary
+    {
+        static void Main(string[] args)
+        {
+            Complex a, b, c ;
+            Complex a1, b1;
+            a = new Complex(2.5, 3.5);
+            b = new Complex(1.6, 2.7);
+            c = a + b;
+            c.Display();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ operatorOverloadingUnary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Space
+{
+    int x, y, z;
+    public Space(int a, int b, int c)
+    {
+        x = a;
+        y = b;
+        z = c;
+    }
+    public void Display()
+    {
+        Console.Write(" "+x );
+        Console.Write(" " + y);
+        Console.Write(" " + z);
+        Console.WriteLine();
+    }
+   /* public static Space operator  - (Space s)
+    {
+        s.x = -s.x;
+        s.y = -s.y;
+        s.z = -s.z;
+        //return (s.x, s.y, s.z);
+    }*/
+}
+namespace advanced
+{
+    class operatorOverloadingUnary
+    {
+        static void Main(string[] args)
+        {
+
+            Space s = new Space(10, -40, 50);
+            Console.Write("s: ");
+            s.Display();
+
+           // -s;
+
+            Console.Write("s: ");
+            s.Display();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
