020b2a3aef9ab9958b359fafd821950add50f867
Sahil-pillania <sahilpillania88@gmail.com>
2022-01-20 12:35:24 +0530

    Assembly

---
+++ Assembly.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using ClassLib ;
+
+namespace advanced
+{
+    class Assembly
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("hello");
+            Class1 one = new Class1();
+            string output1 = one.Funone("Hello");
+            string output2 = one.Funtwo("Hello2");
+            Console.WriteLine(output1);
+            Console.WriteLine(output2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- operatorOverloadingUnary.cs
+++ operatorOverloadingUnary.cs
@@ -1,50 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-class Space
+
+namespace advanced2
 {
-    int x, y, z;
-    public Space(int a, int b, int c)
+    class space
     {
-        x = a;
-        y = b;
-        z = c;
-    }
-    public void Display()
-    {
-        Console.Write(" "+x );
-        Console.Write(" " + y);
-        Console.Write(" " + z);
-        Console.WriteLine();
-    }
-   /* public static Space operator  - (Space s)
-    {
-        s.x = -s.x;
-        s.y = -s.y;
-        s.z = -s.z;
-        //return (s.x, s.y, s.z);
-    }*/
-}
-namespace advanced
-{
-    class operatorOverloadingUnary
-    {
-        static void Main(string[] args)
+        int x, y, z;
+        public space(int a, int b, int c)
         {
-
-            Space s = new Space(10, -40, 50);
-            Console.Write("s: ");
-            s.Display();
-
-           // -s;
-
-            Console.Write("s: ");
-            s.Display();
-
-
+            x = a;
+            y = b;
+            z = c;
+        }
+        public void display()
+        {
+            Console.WriteLine(" " + x);
+            Console.WriteLine(" " + y);
+            Console.WriteLine(" " + z);
+            Console.WriteLine();
+        }
+        public static space operator -(space s)
+        {
+            s.x = -s.x;
+            s.y = -s.y;
+            s.z = -s.z;
+            return s;
+        }
+    }
+    internal class Program
+    {
+        public static void Main()
+        {
+            space s = new space(10, 20, 30);
+            Console.WriteLine("s : ");
+            s.display();
+            s = -s;
+            s.display();
             Console.ReadKey();
 
         }
