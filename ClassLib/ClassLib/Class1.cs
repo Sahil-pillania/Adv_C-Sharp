d08efc6d4a2f1068b64d3555a4c1491414b4108f
Sahil pillania <80634110+Sahil-pillania@users.noreply.github.com>
2022-01-24 12:16:32 +0530

    Add files via upload

---
+++ ClassLib/ClassLib/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class Class1
+    {
+        
+            public string Funone(string text)
+            {
+               return (text);
+            }
+            public string Funtwo(string text)
+            {
+               return ("funtwo called " + text);
+            }
+        
+    }
+}
