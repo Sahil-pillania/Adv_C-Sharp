0db8bca78e3f0514379f4b2ef31877208b3bcf36
Sahil-pillania <sahilpillania88@gmail.com>
2022-02-06 12:03:41 +0530

    commit

--- Assembly.cs
+++ Assembly.cs
@@ -10,8 +10,8 @@ namespace advanced
         {
             //Console.Write("hello");
             Class1 one = new Class1();
-            string output1 = one.Funone("Hello");
-            string output2 = one.Funtwo("Hello2");
+            string output1 = one.Funone("Hello. This is function one");
+            string output2 = one.Funtwo("Hello. This is function two.");
             Console.WriteLine(output1);
             Console.WriteLine(output2);
             Console.ReadKey();
---
+++ ThreadPriority.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+
+
+
+public class Abc
+        {
+            public void A()
+            {
+                Console.WriteLine("thread A");
+                Console.WriteLine("priority of thread A is:" + Thread.CurrentThread.Priority.ToString());
+                Console.WriteLine(" ");
+                Thread.Sleep(5000);
+                Console.WriteLine("thread with" + Thread.CurrentThread.Priority.ToString() + "has run");
+                Console.WriteLine(" ");
+            }
+            public void B()
+            {
+                Console.WriteLine("thread B");
+                Console.WriteLine("priority of thread B is:" + Thread.CurrentThread.Priority.ToString());
+                Console.WriteLine("");
+                Thread.Sleep(4000);
+                Console.WriteLine("thread with" + Thread.CurrentThread.Priority.ToString() + "has run");
+                Console.WriteLine(" ");
+
+            }
+            public void C()
+            {
+                Console.WriteLine("thread C");
+                Console.WriteLine("priority of thread C is:" + Thread.CurrentThread.Priority.ToString());
+                Console.WriteLine(" ");
+                Thread.Sleep(3000);
+                Console.WriteLine("thread with" + Thread.CurrentThread.Priority.ToString() + "has run");
+                Console.WriteLine(" ");
+
+            }
+        }
+  class ThreadPriority
+        {
+            public static System.Threading.ThreadPriority AboveNormal { get; private set; }
+            public static System.Threading.ThreadPriority BelowNormal { get; private set; }
+            public static System.Threading.ThreadPriority Lowest { get; private set; }
+
+             public static void Main()
+            {
+                Console.WriteLine("scheduling thread.......");
+                Abc A1 = new Abc();
+                Thread t1 = new Thread(new ThreadStart(A1.A));
+                Thread t2 = new Thread(new ThreadStart(A1.B));
+                Thread t3 = new Thread(new ThreadStart(A1.C));
+                Console.WriteLine("priority of main method is" + Thread.CurrentThread.Priority.ToString());
+                Console.WriteLine(" ");
+                t1.Name = "thread A";
+                t2.Name = "thread B";
+                t3.Name = "thread C";
+                t1.Priority = ThreadPriority.AboveNormal;
+                t2.Priority = ThreadPriority.BelowNormal;
+                t3.Priority = ThreadPriority.Lowest;
+                t1.Start();
+                t2.Start();
+                t3.Start();
+                Console.ReadKey();
+            }
+        }
+
+    
+
